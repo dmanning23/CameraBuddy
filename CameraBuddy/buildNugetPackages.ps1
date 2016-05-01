e40d8dcaf7052090a21172b2fb0edb40b6bbba97
Dan Manning <dmanning23@hotmail.com>
2016-05-01 13:04:41 -0500

    added winuniproj, updated nuget

--- CameraBuddy/buildNugetPackages.ps1
+++ CameraBuddy/buildNugetPackages.ps1
@@ -1,3 +1,2 @@
-nuget pack .\CameraBuddy.Android\CameraBuddy.Android.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-nuget pack .\CameraBuddy.iOS\CameraBuddy.iOS.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-nuget pack .\CameraBuddy.DesktopGL\CameraBuddy.DesktopGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\CameraBuddy.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget push *.nupkg
